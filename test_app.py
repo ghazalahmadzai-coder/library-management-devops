f532d3b6985ace731f466320cd3585b9b9464714
Raqimafarhad <154404535+Raqimafarhad@users.noreply.github.com>
2026-05-10 21:58:54 +0430

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -1,2 +1,13 @@
-def test_dummy():
-    assert True
+import pytest
+from app import app
+
+@pytest.fixture
+def client():
+    # Flask provides a test client for simulating requests
+    with app.test_client() as client:
+        yield client
+
+def test_homepage(client):
+    # Replace "/" with the route you want to test
+    response = client.get("/")
+    assert response.status_code == 200
